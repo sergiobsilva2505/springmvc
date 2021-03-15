304fd4cbc5d9574a86844324706ae562c447788f
Sergio Bezerra <sergio.bezerra@outlook.com>
2021-03-15 15:35:03 -0300

    Arquivo database.properties

--- src/main/java/br/com/sbs/estacionamento/controller/VeiculoController.java
+++ src/main/java/br/com/sbs/estacionamento/controller/VeiculoController.java
@@ -1,14 +1,8 @@
 package br.com.sbs.estacionamento.controller;
 
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import br.com.sbs.estacionamento.entities.Veiculo;
 
 @Controller
 @RequestMapping("/veiculos")
